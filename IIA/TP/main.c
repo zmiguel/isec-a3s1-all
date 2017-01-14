c9724da3aeaeebd4a0cee7c95272e9edcf4132fd
ZMiguel Alves <zmiguel.alves@gmail.com>
2017-01-14 02:57:38 +0000

    Implementados argumentos de linha de comandos   - permite escolher o ficheiro e/ou o numero de iteraçoes a correr  	modified:   IIA/TP/main.c  	modified:   IIA/TP/main.exe  	modified:   IIA/TP/main.obj

--- IIA/TP/main.c
+++ IIA/TP/main.c
@@ -2,42 +2,71 @@
 #include <stdlib.h>
 #include <time.h>
 #include <string.h>
+#include <stdbool.h>
 #include "util.h"
 #include "util.c"
 
 #define MAX_ITEMS 10000
 
-int main(){
-  int k,i,j,a=1,p,w=0;
-  int str[2000];
-  int conta =0;
-  int contador =0;
-  int pontos;
-  int nova;
-  int auxi[20000];
-  int novoArr[2000];
-  int soma =0;
-  int somar=0;
+int main(int argc, char *argv[]){
+    //variaveis
+  int itera=0, k=0, i=0, j=0, a=0, p=0, w=0, str[MAX_ITEMS], conta=0, contador=0, pontos=0, nova=0, auxi[MAX_ITEMS], novoArr[MAX_ITEMS], soma=0, somar=0, totalSol=0, temp1=0, temp2=0, temp3=0, ii=0, melhor_nr=0;
   char nome_fich[100];
-  int totalSol;
-  int temp1=0, temp2=0, temp3=0;
   struct item item[MAX_ITEMS];
-
-  float melhor, temp4;
-  int ii, melhor_nr=0;
-
+  float melhor=0, temp4=0;
+  bool runs = false, fiile = false;
+    //setup random
   time_t t;
   srand((unsigned)time(&t));
 
-  //perguntar ao utilizador o nome do ficheiro a usar
-  printf("Introduza o nome do ficheiro: ");
-  fgets(nome_fich, sizeof(nome_fich), stdin);
-  nome_fich[strlen(nome_fich)-1]='\0';
+    //verificar argumentos de linha de commandos
+  if(argc==2){
+    printf("Args = 2\n");
+    if(isdigit(*argv[1])){
+      printf("Arg is Digit\n");
+      itera = strtol(argv[1], NULL, 10);
+      runs = true;
+    }else{
+      printf("Arg ISN'T digit\n");
+      memcpy(nome_fich, argv[1], sizeof(nome_fich));
+      //fgets(nome_fich, sizeof(nome_fich), *argv[1]);
+      fiile = true;
+    }
+  }else if(argc==3){
+    printf("Args = 3\n");
+    if(isdigit(*argv[1])){
+      printf("Arg 1 IS digit\n");
+      itera = strtol(argv[1], NULL, 10);
+      memcpy(nome_fich, argv[2], sizeof(nome_fich));
+      //fgets(nome_fich, sizeof(nome_fich), *argv[2]);
+      runs = true;
+      fiile = true;
+    }else{
+      printf("Arg 1 ISN'T digit\n");
+      itera = strtol(argv[2], NULL, 10);
+      memcpy(nome_fich, argv[1], sizeof(nome_fich));
+      //fgets(nome_fich, sizeof(nome_fich), *argv[1]);
+      runs = true;
+      fiile = true;
+    }
+  }else if(argc > 3){
+    printf("Demasiados argumentos ao iniciar o programa %s\n", argv[0]);
+    return 0;
+  }
 
-  totalSol = init_dados(nome_fich);
+  if(runs==false){
+    itera = 50;
+    //numero standard de iteraçoes a correr!
+  }
+  if(fiile==false){
+    //perguntar ao utilizador o nome do ficheiro a usar
+    printf("Introduza o nome do ficheiro: ");
+    fgets(nome_fich, sizeof(nome_fich), stdin);
+    nome_fich[strlen(nome_fich)-1]='\0';
+  }
 
-  for(ii=0;ii<500;ii++){
-      //abrir ficheiro aqui
+  //ciclo para analisar dados
+  for(ii=0;ii<itera;ii++){
 
       totalSol = init_dados(nome_fich);
 
@@ -58,7 +87,7 @@ int main(){
       printf("\n"); // cenas
       //cria array sem numeros aleatorios repetidos
       for(k=0;0==0;k++){
-        p =  give_rand(1,max);//1+rand()%(max+1);//meter max aqui
+        p =  give_rand(1,max);
         novoArr[k] = p;
 
         for(a = 0; a<max; a++){
@@ -122,8 +151,8 @@ int main(){
         melhor = temp4;
         melhor_nr = pontos;
       }
-      printf("Distancia media para %d pontos, iteracao %d: %f\n", pontos, ii, temp4);
-      for(i=0;i<2000;i++){//reset tudo a zero
+      printf("Distancia media para %d pontos, iteracao %d: %f\n", pontos, ii+1, temp4);
+      for(i=0;i<MAX_ITEMS;i++){//reset tudo a zero
         novoArr[i]=0;
         str[i]=0;
         auxi[i]=0;
